92444f0e411387e8f518453ab2ffd61c67a36d69
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-04-15 21:20:16 +0530

    Update ques2.c

--- ques2.c
+++ ques2.c
@@ -4,12 +4,6 @@
 #include <stdlib.h>
 
 int main(){
-    char input[5];
-    int sum=0;
-    for(int i=0;i<5;i++){
-    scanf(" %c", &input[i]);
-    sum+= input[i];}
-    printf("%d", sum);
-    return 0;
+    
 }
 
